59335b6a89394bc31d0622243e7a79b78a18bc87
Niels Andersen <113096753+NillerA@users.noreply.github.com>
2023-11-15 18:28:09 +0100

    grid serialization working

--- Assets/Scripts/Grid/Grid.cs
+++ Assets/Scripts/Grid/Grid.cs
@@ -1,12 +1,43 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class Grid
+[Serializable]
+public class Grid : ISerializationCallbackReceiver
 {
 
-    [Tooltip("tiles")]
+    [SerializeField, Tooltip("tiles")]
     public GridTile[,] Tiles;
 
+    [SerializeField]
+    private GridTile[] serializedTiles;
+    [SerializeField]
+    private int serializedXAmount;
+
+    public void OnAfterDeserialize()
+    {
+        Tiles = new GridTile[serializedXAmount, serializedTiles.Count() / serializedXAmount];
+        for (int x = 0; x < Tiles.GetLength(0); ++x)
+        {
+            for (int y = 0; y < Tiles.GetLength(1); y++)
+            {
+                Tiles[x, y] = serializedTiles[x * Tiles.GetLength(0) + y];
+            }
+        }
+    }
+
+    public void OnBeforeSerialize()
+    {
+        serializedTiles = new GridTile[Tiles.Length];
+        serializedXAmount = Tiles.GetLength(0);
+        for (int x = 0; x < Tiles.GetLength(0); ++x)
+        {
+            for (int y = 0; y < Tiles.GetLength(1); y++)
+            {
+                serializedTiles[x * Tiles.GetLength(0) + y] = Tiles[x, y];
+            }
+        }
+    }
 }
