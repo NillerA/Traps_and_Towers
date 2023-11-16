e4f257986fab410940ebee30313326964899eb14
Verybigtuna <112692489+Verybigtuna@users.noreply.github.com>
2023-11-16 13:29:05 +0100

    buttons done

--- Assets/BuildManager.cs
+++ Assets/BuildManager.cs
@@ -4,16 +4,7 @@ using UnityEngine;
 
 public class BuildManager : MonoBehaviour
 {
-    //public static BuildManager instance;
-
-    //void Awake()
-    //{
-    //    if (instance != null)
-    //    {
-    //        Debug.LogError("More than one BuildManager instance!");
-    //    }
-    //    instance = this; 
-    //}
+    
 
     public GameObject rangedTurretPrefab;
 
--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -7,13 +7,22 @@ public class Shop : MonoBehaviour
     public MousePosition3D mousePos;
     // Start is called before the first frame update
     private MousePosition3D mousePosition;
-
+    public GameObject ShopUI;
+    public GameManager gameManager;
     public void PurchaseTower(GridTileItem item)
     {
         mousePos.item = item;
         mousePos.StartDrag();
     }
-
+    public void CanPlaceTurret()
+    {
+        ShopUI.SetActive(true);
+    }
+    public void OnPlaceSucces()
+    {
+        ShopUI.SetActive(false);
+        gameManager.TowerPlaced();
+    }
     public void PurchaseRangedTurret()
     {
         mousePos.StartDrag();
