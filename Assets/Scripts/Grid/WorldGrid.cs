878abb35a9e150f31caa8c861f0cf6c5668243f2
Niels Andersen <113096753+NillerA@users.noreply.github.com>
2023-11-16 20:16:45 +0100

    Update WorldGrid.cs

--- Assets/Scripts/Grid/WorldGrid.cs
+++ Assets/Scripts/Grid/WorldGrid.cs
@@ -9,6 +9,8 @@ using UnityEngine.UIElements;
 public class WorldGrid : MonoBehaviour, ISerializationCallbackReceiver
 {
 
+    public static WorldGrid Instance;
+
     [SerializeField]
     private Grid grid;
     [SerializeField, HideInInspector]
@@ -22,6 +24,14 @@ public class WorldGrid : MonoBehaviour, ISerializationCallbackReceiver
     public Map map;
     [SerializeField, Range(0,100)]
     private int xAmount, yAmount;
+
+    WorldGrid()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(this);
+    }
 
     public void Generate()
     {
