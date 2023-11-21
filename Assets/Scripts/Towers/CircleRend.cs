af8d580d135fcc2cd56f5024c65f82554a24b9ec
NillerA <113096753+NillerA@users.noreply.github.com>
2023-11-21 15:50:24 +0100

    more updates for circle rend

--- Assets/Scripts/Towers/CircleRend.cs
+++ Assets/Scripts/Towers/CircleRend.cs
@@ -15,7 +15,7 @@ public class CircleRend : MonoBehaviour
     {
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.positionCount = pointCount + 1;
-        lineRenderer.widthMultiplier = 0f;
+        lineRenderer.widthMultiplier = 0.1f;
     }
 
     private void Update()
@@ -30,7 +30,7 @@ public class CircleRend : MonoBehaviour
         for (int i = 0; i <= pointCount; i++)
         {
             float angle = i * angle_between_points * Mathf.Deg2Rad;
-            Vector3 direction = new Vector3(Mathf.Sin(angle), Mathf.Cos(angle), 0f);
+            Vector3 direction = new Vector3(Mathf.Sin(angle), 0.15f, Mathf.Cos(angle));
             Vector3 position = direction * radius;
 
             lineRenderer.SetPosition(i, position);
