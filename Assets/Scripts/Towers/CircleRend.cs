64b2256c691879473e10958bde2862f77ecf8224
Niels Andersen <113096753+NillerA@users.noreply.github.com>
2023-11-21 08:19:39 +0100

    circle rend

---
+++ Assets/Scripts/Towers/CircleRend.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleRend : MonoBehaviour
+{
+
+    [SerializeField]
+    private int pointCount;
+    [SerializeField]
+    private float radius;
+    private LineRenderer lineRenderer;
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = pointCount + 1;
+        lineRenderer.widthMultiplier = 0f;
+    }
+
+    private void Update()
+    {
+        Draw();
+    }
+
+    void Draw()
+    {
+        float angle_between_points = 360f / pointCount;
+
+        for (int i = 0; i <= pointCount; i++)
+        {
+            float angle = i * angle_between_points * Mathf.Deg2Rad;
+            Vector3 direction = new Vector3(Mathf.Sin(angle), Mathf.Cos(angle), 0f);
+            Vector3 position = direction * radius;
+
+            lineRenderer.SetPosition(i, position);
+        }
+    }
+}
