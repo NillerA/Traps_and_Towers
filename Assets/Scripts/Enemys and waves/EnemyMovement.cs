6d7eda2172b146589e95fcc49a94f2cc265f8f99
Niels Andersen <113096753+NillerA@users.noreply.github.com>
2023-11-21 23:38:35 +0100

    Enemy Healthbar

--- Assets/Scripts/Enemys and waves/EnemyMovement.cs
+++ Assets/Scripts/Enemys and waves/EnemyMovement.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyMovement : MonoBehaviour
 {
@@ -9,6 +11,11 @@ public class EnemyMovement : MonoBehaviour
     public int currentHealth;
 
     public int damage;
+    [SerializeField]
+    private Slider healthSlider;
+    [SerializeField]
+    private GameObject healthBarSeperator;
+    private List<GameObject> healthBarSeperators;
 
     int currentPathTarget=0;
     [SerializeField]
@@ -23,9 +30,7 @@ public class EnemyMovement : MonoBehaviour
             direction = new Vector3(path[currentPathTarget].x - gameObject.transform.position.x,0, path[currentPathTarget].z - gameObject.transform.position.z);
 
             if (direction.magnitude <= 0.1f)
-            {
                 currentPathTarget += 1;
-            }
             else
             {
                 direction.Normalize();
@@ -33,14 +38,13 @@ public class EnemyMovement : MonoBehaviour
             }
         }
         else
-        {
             WaveManager.Instance.ReachedBase(gameObject);
-        }
     }
 
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
+        healthSlider.value = currentHealth;
         if (currentHealth <= 0)
         {
             WaveManager.Instance.ReleaseEnemy(gameObject);
@@ -55,6 +59,14 @@ public class EnemyMovement : MonoBehaviour
         stats = newStats;
         enemyName = stats.name;
         currentHealth = stats.maxHealth;
+        healthSlider.maxValue = stats.maxHealth;
+        healthSlider.value = stats.maxHealth;
+        if (healthBarSeperators != null)
+            for (int i = 0; i < healthBarSeperators.Count; i++)
+                Destroy(healthBarSeperators[i]);
+        healthBarSeperators = new List<GameObject>();
+        for (int i = 1; i < stats.maxHealth; i++) 
+            healthBarSeperators.Add(Instantiate(healthBarSeperator, healthBarSeperator.transform.parent));
 
         if (enemyVisual != null)
             Destroy(enemyVisual);
