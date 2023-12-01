95f03a0c2be037ed4082c102698887c62365da08
NillerA <113096753+NillerA@users.noreply.github.com>
2023-12-01 10:53:31 +0100

    sniper tower buff

--- Assets/Scripts/Grid/GridManager.cs
+++ Assets/Scripts/Grid/GridManager.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 using UnityEngine;
 
 public class GridManager : MonoBehaviour, ISerializationCallbackReceiver
